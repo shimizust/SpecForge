4a6a1771d578bb23dc60a9cca467098904dde13e
Steven Shimizu <sshimizu@linkedin.com>
2025-08-14 18:11:36 -0700

    Fixed lint

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 def read_requirements():
     with open(f"requirements.txt", "r") as f:
         lines = (line.strip() for line in f)
-        return [line for line in lines if line and not line.startswith(('#', '--'))]
+        return [line for line in lines if line and not line.startswith(("#", "--"))]
 
 
 def read_readme():
